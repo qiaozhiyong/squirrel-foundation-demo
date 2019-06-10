1f65305de7cf699d44f10dd1169c80cc1cbf82a5
qiaozhy <zhiyong.qiao@qworks.id>
2019-06-10 11:44:08 +0800

    qiao11

--- src/main/java/com/lyxiang/SquirrelApplication.java
+++ src/main/java/com/lyxiang/SquirrelApplication.java
@@ -4,11 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.lyxiang"})
 @MapperScan(basePackages = "com.lyxiang.dao")
-//@EnableSpringConfigured
+@EnableSpringConfigured
 public class SquirrelApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lyxiang/service/OrderService.java
+++ src/main/java/com/lyxiang/service/OrderService.java
@@ -5,8 +5,14 @@ import com.lyxiang.common.OrderEvent;
 import com.lyxiang.common.OrderState;
 import com.lyxiang.pojo.OrderDTO;
 import com.lyxiang.spring.OrderStateMachineEngineFactory;
+import com.lyxiang.squirrelfoundation.MyEvent;
+import com.lyxiang.squirrelfoundation.MyState;
+import com.lyxiang.squirrelfoundation.declarative.DeclarativeStateMachine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.squirrelframework.foundation.fsm.StateMachineBuilderFactory;
+import org.squirrelframework.foundation.fsm.UntypedStateMachine;
+import org.squirrelframework.foundation.fsm.UntypedStateMachineBuilder;
 
 /**
  * @author: liyuxiang
@@ -18,15 +24,34 @@ public class OrderService {
     @Autowired
     OrderStateMachineEngineFactory orderStateMachineEngineFactory;
 
-    public int submitOrder() {
+    /*public int submitOrder() {
         OrderDTO orderDTO = new OrderDTO(OrderState.INIT.name());
         OrderContext orderContext = new OrderContext(orderDTO);
         orderStateMachineEngineFactory.fire(orderContext, OrderEvent.SUBMIT_ORDER);
-       /* OrderDTO orderDTO2 = new OrderDTO(OrderState.WAIT_PAY.name());
+       *//* OrderDTO orderDTO2 = new OrderDTO(OrderState.WAIT_PAY.name());
         OrderContext orderContext2 = new OrderContext(orderDTO2);
-        orderStateMachineEngineFactory.fire(orderContext2, OrderEvent.PAY);*/
+        orderStateMachineEngineFactory.fire(orderContext2, OrderEvent.PAY);*//*
         return 1;
         //return orderContext.orderDTO.getId();
+    }*/
+
+    public int submitOrder() {
+        UntypedStateMachineBuilder builder = StateMachineBuilderFactory.create(DeclarativeStateMachine.class);
+
+        // ConverterProvider.INSTANCE.register(MyEvent.class, new MyEventConverter());
+        //ConverterProvider.INSTANCE.register(MyState.class, new MyStateConverter());
+
+        UntypedStateMachine fsm = builder.newStateMachine(MyState.A);
+
+        fsm.start();
+
+        fsm.fire(MyEvent.ToB);
+
+        fsm.fire(MyEvent.ToC);
+
+        fsm.terminate();
+
+        return 1;
     }
 
 }
